b78a240497a35e803c2de23194e7713a592fbdcd
Aleksandr Kuzminsky <aleks@twindb.com>
2016-09-30 10:01:42 -0700

    Revert "Add support for extra curl parameters"

--- scripts/raingauge_package_and_send.sh
+++ scripts/raingauge_package_and_send.sh
@@ -15,8 +15,8 @@ fi
 
 PORT=$(mysql -h localhost -e "select @@port" -ss $userPassArgs)
 DATE=$(ls -r "$PT_STALK_COLLECT_DIR" | tail -n1 | cut -d'-' -f1)
-FILE="/tmp/$HOSTNAME-$DATE.tar.gz"
-URL="${RG_WEB_SERVER}/index.php?action=upload&hostname=$HOSTNAME&port=$PORT"
+FILE="$HOSTNAME-$DATE.tar.gz"
+URL="http://${RG_WEB_SERVER}/RainGauge/index.php?action=upload&hostname=$HOSTNAME&port=$PORT"
 
 #echo "$FILE"
 if [[ ! "$DATE" =~ "^[0-9][0-9][0-9][0-9]" ]];
@@ -26,7 +26,7 @@ fi
 
 tar -zcf "$FILE" -C "$PT_STALK_COLLECT_DIR" .
 if [[ -r "$FILE" ]]; then
-        rm -f "$PT_STALK_COLLECT_DIR"/*
-	curl -F "file=@$FILE" $RG_CURL_PARAMS "$URL"
-        rm -f "$FILE"
+        rm "$PT_STALK_COLLECT_DIR"/*
+	curl -F "file=@$FILE" "$URL"
+        rm "$FILE"
 fi
